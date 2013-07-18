bf9f06728ad1dc1d5662dc9617bd5a67396dfb8c
Fidelb <fidelb@gmail.com>
2013-07-18 11:12:39 +0000

    web.js reads form index.html 2

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
   var buf = new Buffer(50);
   var fs = require('fs');
-  response.send(buf.toString(fs.readFileSync('index.html'));
+  response.send(buf.toString(fs.readFileSync('index.html')));
 });
 
 var port = process.env.PORT || 5000;
